2a12eb9a3139b144e1efab37c7231244d377bf79
marki2121 <mmaruna2013@gmail.com>
2022-03-01 12:52:37 +0100

    Dodan file_save

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ rez_cpu = numpy.empty_like(m)
 
 rez_cpu = m
 
+#Kreiranje matrice
 def rand_unos():
     seed(1)
     for i in range(V):
@@ -34,6 +35,8 @@ rand_unos()
 print(m)
 print("\n\n\n")
 
+
+#CPU solution
 t1 = time.time()
 for k in range(V):
     for i in range(V):
@@ -46,11 +49,14 @@ print("CPU: \n")
 print(rez_cpu)
 print("\n Vrijeme: " + str(t2-t1))
 
+numpy.savetxt("outfile_cpu.txt", rez_cpu, fmt="%2.0f")
+
 mod = SourceModule(open("main.cu").read())
 magic = mod.get_function("funkcija")
 
 block = V / 32
 
+#Racunanje velicine blokova i gridova
 if (block > 1):
     if(block % 2 != 0):
         block = int(block) + 1
@@ -61,6 +67,7 @@ else:
 print("\n\nBlock size (" + str(V), ", " + str(V) + ", 1)")
 print("Grid size (" + str(int(block)) + ", " + str(int(block)) + ")")
 
+#GPU solution
 t1 = time.time()
 for k in range(V):
     magic(drv.InOut(m), drv.In(VV), drv.In(numpy.int32(k)), block=(int(V), int(V), 1), grid=(int(block), int(block)))
@@ -69,3 +76,5 @@ t2 = time.time()
 print("\n\n GPU: \n")
 print(m)
 print("\n Vrijeme: " + str(t2-t1))
+
+numpy.savetxt("outfile_gpi.txt", m, fmt="%2.0f")
